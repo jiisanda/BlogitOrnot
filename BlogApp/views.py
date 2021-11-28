3c0fa0036f1e360c53dd9ce652907e81a12e2df8
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2021-11-28 17:20:34 +0530

    'post-added'

---
+++ BlogApp/views.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name='BlogApp\home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'BlogApp\post_detail.html'
+
