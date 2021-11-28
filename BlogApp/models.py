b9f6466d0fdfdf4bbb745d20f4a8321d48cebb36
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2021-11-29 00:05:06 +0530

    'form-operations'

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=200)
@@ -11,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
+    
