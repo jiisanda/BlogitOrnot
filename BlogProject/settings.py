8e0b70cd3eeeac351280e13de563b3b84ac8f924
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2021-11-29 13:50:52 +0530

    seperated the environment variables

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -1,3 +1,4 @@
+from .env import DEBUG, SECRET_KEY, ALLOWED_HOSTS
 """
 Django settings for BlogProject project.
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fxfp)uyy9uw2mk6r5k&@9!1#tdc_ve0m&0oo1r=c8v(kv2ck6f'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ALLOWED_HOSTS, '127.0.0.1']
 
 
 # Application definition
