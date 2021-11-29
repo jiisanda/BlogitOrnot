1071f1af01e52d09dde966c2fb9f60652b420a1b
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2021-11-29 22:56:11 +0530

    Login-Page-added

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -127,3 +127,7 @@ STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]        # new
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'
+
