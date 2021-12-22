098a29f68be551c7a8e216222ba204d9f1312d3a
heyharshjaiswal <harshjaiswal2307@gmail.com>
2021-12-23 00:01:19 +0530

    Bloh home

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -126,7 +126,7 @@ STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]        # new
 
 # Media Files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'BlogApp/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
