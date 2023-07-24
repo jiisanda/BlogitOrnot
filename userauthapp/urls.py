880e80318100c7c0584c0bba155136ea8df9fe72
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-07-24 13:44:56 +0530

    update userauthapp

--- userauthapp/urls.py
+++ userauthapp/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from userauthapp import views
-from .views import ProfileEditView, PasswordsChangeView, PasswordSuccessView, ShowProfilePageView
-from .views import EditProfilePageView
 
-# app_name="UserAuthApp"
-# TEMPLATE URLS
+from .views import (
+    ProfileEditView, PasswordsChangeView, PasswordSuccessView, ShowProfilePageView, EditProfilePageView, UserLoginView, RegisterView
+)
+
+
 urlpatterns = [
-    path('user_login/', views.user_login, name="user_login"),
-    path('register/', views.registrationView, name='register'),
+    path('login/', UserLoginView.as_view(), name="user_login"),
+    path('register/', RegisterView.as_view(), name='register'),
     path('edit_profile/', ProfileEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(), name='change_password'),
-    path('password_success/', views.PasswordSuccessView, name='password_changed'),
+    path('password_success/', PasswordSuccessView, name='password_changed'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
     path('<int:pk>/edit_user_profile/', EditProfilePageView.as_view(), name='edit_profile_page'),
 ]
--- userauthapp/views.py
+++ userauthapp/views.py
@@ -1,34 +1,39 @@
-from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import get_object_or_404, render
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import hashers as hasher
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import View, generic
+from django.urls import reverse
 from django.urls.base import reverse_lazy
-from userauthapp.forms import UserForm, UserProfileInfoForm, PasswordChangingForm
-from django.views import generic
+
 from .forms import ProfileEditForm, UserProfileEditForm
-from django.views.generic import DetailView
 from .models import Profile
+
 from BlogApp.models import Post
-import django.contrib.auth.hashers as hasher
+from userauthapp.forms import UserForm, UserProfileInfoForm, PasswordChangingForm
+
 
 # Create your views here
+class UserLogoutView(View, LoginRequiredMixin):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('home'))
 
 
-@login_required
-def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('home'))
+class UserLoginView(View):
+    template_name = 'userauthapp/login.html'
 
-def user_login(request):
-    if request.method == 'POST':
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+    def post(self, request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = authenticate(username=username, password=password)
-
-        if user:
+        if user := authenticate(username=username, password=password):
             if user.is_active:
                 login(request, user)
                 return HttpResponseRedirect(reverse('home'))
@@ -36,19 +41,32 @@ def user_login(request):
             else:
                 return HttpResponse("ACCOUNT NOT ACTIVE")
         else:
-            print("Someone try to logon and failed...")
-            print("User: {} and password {}".format(username,password))
             return HttpResponse("Invalid login details supplied!")
-    else:
-        return render(request, 'userauthapp/login.html', {})
 
 
-def registrationView(request):
+class RegisterView(View):
+    template_name = 'userauthapp/registration.html'
+
     registered = False
-    if request.method == "POST":
+
+    def get(self, request):
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': self.registered,
+        }
+
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileInfoForm(data=request.POST)
+
         if user_form.is_valid() and profile_form.is_valid():
+            # self._extracted_from_post(request, user_form=user_form, profile_form=profile_form)
             user = user_form.save()
             user.set_password(user.password)
             user.save()
@@ -59,17 +77,17 @@ def registrationView(request):
             if 'profile_picture' in request.FILES:
                 profile.profile_picture = request.FILES['profile_picture']
             profile.save()
-            registered = True
+            self.registered = True
         else:
             print(user_form.errors, profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileInfoForm()
-    
-    return render(request, 'userauthapp/registration.html',
-                            {'user_form':user_form,
-                            'profile_form':profile_form,
-                            'registered':registered})
+
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': self.registered,
+        }
+
+        return render(request, self.template_name, context=context)
 
 
 class ProfileEditView(generic.UpdateView):
@@ -91,7 +109,7 @@ class PasswordsChangeView(PasswordChangeView):
 def PasswordSuccessView(request):
     return render(request, 'userauthapp/password_success.html', {})
 
-class ShowProfilePageView(DetailView):
+class ShowProfilePageView(generic.DetailView):
     model = Profile
     template_name = 'userauthapp/user_profile.html'
 
@@ -113,5 +131,3 @@ class EditProfilePageView(generic.UpdateView):
     # fields = ['bio', 'profile_picture', 'portfolio_site', 'github_username', 'github_url', 'twitter_username', 'twitter_url', 'instagram_username', 'instagram_url', 'facebook_username', 'facebook_url']
     
     success_url = reverse_lazy('home')
-
-    
